1d4a85115dd0623760af745f14f71d345f9919e5
rajatchakraborty-229 <rajatchakraborty229@gmail.com>
2024-06-30 11:49:48 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,43 @@
+import path from 'path';
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-dotenv.config(); // Load environment variables
+import cors from 'cors';
 
+dotenv.config(); // Load environment variables
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import userRoutes from "./routes/user.route.js"
+import userRoutes from "./routes/user.route.js";
 
-import { app, server, } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
-app.use(express.json());
-app.use(cookieParser())
 
+const __dirname = path.resolve();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors()); // Enable CORS for all routes
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
 server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on ${PORT}`);
 });
-
